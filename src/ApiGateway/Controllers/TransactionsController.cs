70934ee8312e23230e4e7812c771a2807e07d9ce
MiguelcrReis <miguelcr.reis@gmail.com>
2026-01-03 14:31:41 -0300

    feat: add create transaction request contract

---
+++ src/ApiGateway/Controllers/TransactionsController.cs
@@ -0,0 +1,28 @@
+﻿using ApiGateway.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("api/transactions")]
+    public class TransactionsController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateTransactionRequest request)
+        {
+            Log.Information(
+                "Received transaction request | AccountId={AccountId} Amount={Amount} Currency={Currency}",
+                request.AccountId,
+                request.Amount,
+                request.Currency
+            );
+
+            return Accepted(new
+            {
+                Message = "Transaction accepted for processing",
+                CorrelationId = HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
---
+++ src/ApiGateway/DTOs/CreateTransactionRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway.DTOs
+{
+    public class CreateTransactionRequest
+    {
+        public string AccountId { get; set; } = null!;
+        public decimal Amount { get; set; }
+        public string Currency { get; set; } = "BRL";
+        public string Description { get; set; } = string.Empty;
+    }
+}
